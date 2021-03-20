f4a3c8fe5be7a2f699024327fe1edeb0600f9a24
Sherly1001 <Sherly1001@users.noreply.github.com>
2021-03-20 17:02:45 +0700

    w4

---
+++ w4/AimsProject/Aims.java
@@ -0,0 +1,47 @@
+package AimsProject;
+
+
+public class Aims {
+	public static void main(String[] args) {
+		Order anOrder = Order.getInstanceOrder();
+		Order twoOrder = Order.getInstanceOrder();
+		Order threeOrder = Order.getInstanceOrder();
+		Order fourOrder = Order.getInstanceOrder();
+		Order fiveOrder = Order.getInstanceOrder();
+		Order sixOrder = Order.getInstanceOrder();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+		dvd1.setCategory("Animatin");
+		dvd1.setCost(19.95f);
+		dvd1.setDirector("Roger Allers");
+		dvd1.setLength(87);
+		anOrder.addDigitalVideoDisc(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Start Wars");
+		dvd2.setCategory("Science Fiction");
+		dvd2.setCost(24.95f);
+		dvd2.setDirector("George Lucas");
+		dvd2.setLength(124);
+		anOrder.addDigitalVideoDisc(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin");
+		dvd3.setCategory("Animatin");
+		dvd3.setCost(18.99f);
+		dvd3.setDirector("Jonh Musker");
+		dvd3.setLength(90);
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		System.out.print("Total cost is: ");
+		System.out.println(anOrder.totalCost());
+		
+		// check remove disc
+		anOrder.removeDigitalVideoDisc(dvd2);
+		System.out.println(anOrder.totalCost());
+		
+		anOrder.printOrdered();
+		twoOrder.printOrdered();
+		threeOrder.printOrdered();
+		fourOrder.printOrdered();
+		fiveOrder.printOrdered();
+	}
+}
---
+++ w4/AimsProject/DateTest.java
@@ -0,0 +1,45 @@
+package AimsProject;
+
+import junit.framework.TestCase;
+
+public class DateTest extends TestCase {
+	public static void main(String[] args) {
+		// MyDate date1 = new MyDate();
+		// testGetDay(14, date1);
+		// testGetMonth(3, date1);
+		// testGetYear(2021, date1);
+		// testSetYear(-2001, date1);
+		// testAccept(13, 11, 2020, date1);
+
+		MyDate aDate = new MyDate("first", "February", "twenty nineteen");
+		MyDate bDate = new MyDate("first", "February", "twenty twenty");
+		MyDate[] dateList = {bDate, aDate};
+		aDate.print();
+		bDate.print();
+		System.out.printf("aDate vs bDate is: %d\n",
+				DateUtils.compareMyDate(aDate, bDate));
+		System.out.println("Before sorting");
+		DateUtils.print(dateList);
+		DateUtils.sortMyDate(dateList);
+		System.out.println("After sorting");
+		DateUtils.print(dateList);
+	}
+	public static void testGetDay(int day, MyDate d) {
+		assertEquals(day, d.getDay());
+	}
+	public static void testGetMonth(int month, MyDate d) {
+		assertEquals(month, d.getMonth());
+	}
+	public static void testGetYear(int year, MyDate d) {
+		assertEquals(year, d.getYear());
+	}
+	public static void testSetYear(int year, MyDate d) {
+		d.setYear(year);
+		assertEquals("Invalid Year", year, d.getYear());
+	}
+	public static void testAccept(int day, int month, int year, MyDate d) {
+		d.accept();
+		assertTrue(d.getDay() == day && d.getMonth() == month
+				&& d.getYear() == year);
+	}
+}
